47d4fd660959ee7cafdcfb9efdb16bb125934cfa
AYOUB CHOUKRI <ayoubchoukri16@gmail.com>
2022-03-30 20:26:37 +0000

    kilina  30/03/2022

--- adminstartion/Dashboard/gestionRH.php
+++ adminstartion/Dashboard/gestionRH.php
@@ -15,6 +15,18 @@ include 'header.php';?>
         width: 150px;
         height: 250px;
     }
+    .cv{
+        position: absolute;
+        top: 10%;
+        left: 30%;
+        right: 30%;
+        width: 40%;
+        height: 80%;
+        padding: 10px;
+    }
+    #cv{
+        display: none;
+    }
     </style>
     <script>
     document.title = 'Gestion du Recrutement';
@@ -22,17 +34,11 @@ include 'header.php';?>
 
     <div class="wrapper">
         <?php include 'menu.php';?>
-
-
         <div class="main">
             <?php include 'Top.php';?>
-
             <main class="content">
                 <div class="container-fluid p-0">
                     <h1 class="h3 mb-3"><strong>Gestion du Recrutement</strong></h1>
-
-
-
                     <div class="row">
                         <div class="col-xl col-xxl ">
                             <div class="w-100">
@@ -51,18 +57,12 @@ include 'header.php';?>
                                                         </tr>
                                                     </thead>
                                                     <tbody id="myTable">
-                                                        <?php $sql="SELECT * FROM `recrutement` ";
+                                                        <?php $sql="SELECT * FROM `recrutement` order by `id` DESC ";
                                                               $result = $cnx->query($sql);
                                                               while ($row = $result->fetch_assoc()) {
                                                         ?>
                                                         <tr>
-                                                            <!-- <td class="text-left"><img src="<?php echo 'data:file/pdf;base64,' . base64_encode($row['cv']); ?>"
-                                                                                       width="20" height="20"  alt=""></td> -->
-                                                            <td class="text-left">
-                                                                <iframe src="<?php echo $row['cv'].'pdf'; ?>">
-
-                                                                </iframe>
-                                                            </td>
+                                                            <td class="text-left"><button onclick="afficher('<?php echo 'data:application/pdf;base64,' . base64_encode($row['cv']);?>')">Voir CV</button></td>
                                                             <td class="text-left"><?php echo $row['nom'];?></td>
                                                             <td class="text-left"><?php echo $row['prenom'];?></td>
                                                             <td class="text-left"><?php echo $row['tele'];?></td>
@@ -78,17 +78,24 @@ include 'header.php';?>
                             </div>
                         </div>
                     </div>
-                
-
-
-
-
-
                 </div>
             </main>
+            <div id="cv" class="cv bg-light p-2">
+                <div class="text-end bg-white"><button onclick="fermer()"><i class="align-middle" data-feather="x-square"></i></button></div>
+                <embed id="cvT" src="" width="100%" height="100%" type='application/pdf'/>
+            </div>
 
         </div>
     </div>
+    <script>
+        function afficher(text) { 
+            document.getElementById('cv').style.display='block';
+            document.getElementById('cvT').src=text;
+         }
+         function fermer(){
+            document.getElementById('cv').style.display='none';
+         }
+    </script>
 
 
     <?php include 'script.php';?>
