4ad683cc700d2409a24a80e938674d0022c1c3ed
AYOUB CHOUKRI <ayoubchoukri16@gmail.com>
2022-05-12 17:31:34 +0100

    kilina  12/05/2022

---
+++ addPanier.php
@@ -0,0 +1,52 @@
+<?php 
+session_start();
+$_SESSION['titre1']='Ajouter un produit';
+// $_SESSION['panier'] = array();
+// $_SESSION['panier']['id'] = array();
+// $_SESSION['panier']['titre'] = array();
+// $_SESSION['panier']['prix'] = array();
+// $_SESSION['panier']['quantite'] = array();
+// $_SESSION['panier']['image'] = array();
+// $_SESSION['panier']['total'] = 0;
+
+
+class Panier {
+    public $id;public $titre;public $prix;public $quantite;public $image;public $total;
+
+    function set_id($id) { $this->id = $id; } function get_id() { return $this->id; }
+
+    function set_titre($titre) { $this->titre = $titre; } function get_titre() { return $this->titre; }
+
+    function set_prix($prix) { $this->prix = $prix; } function get_prix() { return $this->prix; }
+
+    function set_quantite($quantite) { $this->quantite = $quantite; } function get_quantite() { return $this->quantite; }
+
+    function set_image($image) { $this->image = $image; } function get_image() { return $this->image; }
+
+    function set_total($total) { $this->total = $total; } function get_total() { return $this->total; }
+  
+
+  }
+  $panier1=new Panier();
+    $panier1->set_id(1);
+    $panier1->set_titre("Titre 1");
+    $panier1->set_prix(10);
+    $panier1->set_quantite(1);
+    $panier1->set_image("image1.jpg");
+    $panier1->set_total(10);
+    // array_push($_SESSION['panier']['id'],$panier1->get_id());
+    // array_push($_SESSION['panier']['titre'],$panier1->get_titre());
+    // array_push($_SESSION['panier']['prix'],$panier1->get_prix());
+    // array_push($_SESSION['panier']['quantite'],$panier1->get_quantite());
+    // array_push($_SESSION['panier']['image'],$panier1->get_image());
+    // array_push($_SESSION['panier']['total'],$panier1->get_total());
+
+    
+  
+
+
+
+
+
+
+?>
--- navbar.php
+++ navbar.php
@@ -308,7 +308,7 @@
 <style>
     .ListeCmd{
         position: absolute;
-        top: 0;
+        top: 1%;
         right: 0;
         width: 460px;
         min-height: 100%;
@@ -318,8 +318,8 @@
         box-shadow: 0px 0px 23px -2px #000000;
     }
     .ImgProd{
-        width: 100px;
-        height:150px
+        /* width: 100px; */
+        height:100px
     }
     .detaileCmd > p{
         font-size: 15px;
@@ -328,56 +328,133 @@
     ol li {
         list-style: none;
     }
+    .lien a{
+        text-decoration: none;
+        color:  #bfbfbf;
+    }
+    .lien a:hover{
+        text-decoration: underline;
+        color:  #bfbfbf;
+    }
+    .btn-close{
+        border: none;
+        background: transparent;
+        color: #000;
+        size: 15px;
+        border-radius: 50%;
+       
+    }
+    .btn-close:hover{
+        color: red;
+        size: 15px;
+
+    }
+
 
     
     @media screen and (max-width:700px){
         .ListeCmd{
-            left: 5%;
-            right: 5%;
-            margin: auto;
-            width: 95%;
+           
+            width: 100%;
             height: 100%;
         }
     }
 </style>
 <div class="ListeCmd bg-light">
+    <?php ?>
     <div class="container">
         <div class="row">
             <div class="col-md-12">
                 <div class=" row">
-                        <h5 class="card-title text-left col-sm p-1">Votre panier () </h5><h5 class="col-sm-3 p-1 text-right">X</h5>
+                        <h5 class="card-title text-left col-sm p-1">Votre panier () </h5>
+                        <button onclick="fermerPanier()" type="button" class="btn-close p-2" aria-label="Close">X</button>
+                        <!-- <h5 class="col-sm-3 p-1 text-right">X</h5> -->
                 </div>
                 <ol>
+                    <?php 
+                    $p=10;
+                        for($x=0;$x<5;$x++){
+                             ?>
                     <li >
                         <div class="row">
                             <div class="col-sm-4">
                                 <img src="image/prod.jpg" class="ImgProd" alt="...">
                             </div>
                             <div class="col-sm detaileCmd">
-                                <h4>PANTALON DROIT UNI</h4>
-                                <h6 style="color:red;">199.00 Dhs </h6>
-                                <!-- <p>Taille : S</p> -->
-                                <span>Taille : S</span>
-                                <p>Quantité: 1</p>
+                                <h4><?php echo '->'.$_SESSION['titre1'];?></h4>
+                                <h6  style="color:red;"><span class="prixproduit"><?php echo $p=$p+10;?></span></h6>
+                                <p>Taille :   <span class="taille">S</span></p>
+                                <p >Quantité: <span class="qnt"><?php echo $x+1;?></span></p>
 
                             </div>
                         </div>
+                        <div class="text-center lien">
+                            <a href="">Modifier</a>
+                            <a href="">Supression</a>
+                        </div>
                         <hr>
                     </li>
+                    <?php } ?>
                 </ol>
         
                 
             </div>
         </div>
     </div>
-    <div style="position: absolute ; bottom: 0px;">
+    
+    <div class=" p-2" >
+    <hr>
         <div class="row">
             <div class="col"> <h5 class="text-left">Sous-total :</h5> </div>
-            <div class="col"> <h5 class="text-right">428,00 DHS</h5> </div>
+            <div class="col"> <h5 id="prixprod" class="text-right">428,00 DHS</h5> </div>
         </div>
         <p class="text-center" >Toutes vos informations sont en sécurité et protégées</p>
-
     </div>
 
 
 </div>
+<script>
+
+    let listeprix = document.getElementsByClassName("prixproduit");
+    let listqnt = document.getElementsByClassName("qnt");
+    var somme = 0;
+    for (i = 0; i < listeprix.length; i++) {
+        somme = somme + parseFloat(listeprix[i].innerHTML) * listqnt[i].innerHTML;
+    }
+    document.getElementById("prixprod").innerHTML = somme.toFixed(2);
+
+    function fermerPanier() {
+        document.getElementsByClassName("ListeCmd")[0].style.display = "none";
+        document.getElementsByClassName("ListeCmd")[0].style.transform = "translateX(-100%)";
+        document.getElementsByClassName("ListeCmd")[0].style.transition = "all 2s";
+    }
+</script>
+<!-- <script>
+    /*<![CDATA[*/
+    let listeprix = document.getElementsByClassName("prixproduit");
+    let listqnt = document.getElementsByClassName("qnt");
+    var somme = 0;
+    for (i = 0; i < listeprix.length; i++) {
+        somme = somme + parseFloat(listeprix[i].innerHTML) * listqnt[i].value
+    }
+    document.getElementById("prixprod").innerHTML = somme.toFixed(2);
+    var prixlivr = parseFloat(document.getElementById("prixlivr").innerHTML);
+    var prixprod = parseFloat(document.getElementById("prixprod").innerHTML);
+    prixtotale = prixprod + prixlivr;
+    document.getElementById("prixtotale").innerHTML = prixtotale.toFixed(2);
+    document.getElementById("inputprix").value = prixtotale.toFixed(2);
+
+    function claculer() {
+        let listqnt = document.getElementsByClassName("qnt");
+        var a = 0;
+        for (i = 0; i < listeprix.length; i++) {
+            a = a + parseFloat(listeprix[i].innerHTML) * listqnt[i].value
+        }
+        document.getElementById("prixprod").innerHTML = a.toFixed(2);
+        var c = parseFloat(document.getElementById("prixlivr").innerHTML);
+        var b = parseFloat(document.getElementById("prixprod").innerHTML);
+        prixtotale = b + c;
+        document.getElementById("prixtotale").innerHTML = prixtotale.toFixed(2);
+        document.getElementById("inputprix").value = prixtotale.toFixed(2)
+    }; /*]]>*/
+    </script> -->
